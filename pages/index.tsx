402c14da5209f9228347db8d6828965672450a97
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-05-14 12:18:21 +0900

    test Library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import dynmaic from 'next/dynamic'
-import Post from 'templates/home'
+import Library from 'templates/library'
 
 export default function Page() {
   const Layout = dynmaic(() => import('layouts/default'))
   return (
     <Layout>
-      <Post />
+      <Library />
     </Layout>
   )
 }
