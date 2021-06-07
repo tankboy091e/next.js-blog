8f67a8c5c140bdfc8851a084ffd88e30ec7ce39a
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-06-08 02:31:14 +0900

    Fix next seo

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,5 +1,4 @@
 import Footer from 'components/footer'
-import Head from 'next/head'
 import { DEFAULT_TITLE } from 'pages/_app'
 import React from 'react'
 import styles from 'sass/layouts/default.module.scss'
@@ -20,24 +19,22 @@ export default function Layout({
   const _description = description || '오진수의 블로그입니다'
   return (
     <>
-      <Head>
-        <NextSeo
-          title={_title}
-          description={_description}
-          openGraph={{
-            url: 'https://ohjinsu.me',
-            title: _title,
-            type: 'website',
-            images: [
-              {
-                url: 'https://www.ohjinsu.me/favicon.ico',
-              },
-            ],
-            description: _description,
-            site_name: DEFAULT_TITLE,
-          }}
-        />
-      </Head>
+      <NextSeo
+        title={_title}
+        description={_description}
+        openGraph={{
+          url: 'https://ohjinsu.me',
+          title: _title,
+          type: 'website',
+          images: [
+            {
+              url: 'https://www.ohjinsu.me/favicon.ico',
+            },
+          ],
+          description: _description,
+          site_name: DEFAULT_TITLE,
+        }}
+      />
       <main id={mainContainerID} className={styles.main}>
         {children}
       </main>
