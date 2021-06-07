a2a1f4387bbbf558340e841dd6ef6b2d7551ff10
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-06-07 19:16:05 +0900

    Fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,17 @@
+import usePageQuery from 'lib/hooks/page-query'
 import Link from 'next/link'
 import styles from 'sass/components/footer.module.scss'
 
 export default function Footer() {
   const copyright = `ⓒ 2019-${new Date().getFullYear()}. 오진수 all rights reserved`
+  const { category } = usePageQuery()
   return (
     <footer className={styles.container}>
-      <Link href="/">
-        <a href="/" className={styles.home}>Home</a>
-      </Link>
+      {category !== 'home' && (
+        <Link href="/">
+          <a href="/" className={styles.home}>Home</a>
+        </Link>
+      )}
       <span className={styles.copyright}>{copyright}</span>
     </footer>
   )
