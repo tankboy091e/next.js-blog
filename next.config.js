76d955276b9eb830644470daba3f99403c44d63f
tankboy091e <74132743+tankboy091e@users.noreply.github.com>
2021-07-17 21:31:31 +0900

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /* eslint-disable global-require */
 module.exports = {
-  excludeFile: (str) => /\script\*.js/.test(str),
-  webpack: (config, {
-    isServer,
-  }) => {
-    if (isServer) {
-      require('./script/sitemap.js')
-    }
-    return config
-  },
   env: {
     ORIGIN: process.env.ORIGIN,
     API_URL: process.env.API_URL,
